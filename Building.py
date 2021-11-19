1145445fb383e5cccdeeaafd78256f4a9f0f12b7
YehielSiri <57964066+YehielSiri@users.noreply.github.com>
2021-11-19 10:36:27 +0200

    Add files via upload

--- Building.py
+++ Building.py
@@ -3,28 +3,27 @@ from Elevator import *
 
 class Building:
     def __init__(self, minFloor, maxFloor):
-        self.minFloor = minFloor
-        self.maxFloor = maxFloor
-        self.elevators = []
+        self._minFloor = minFloor
+        self._maxFloor = maxFloor
+        self._elevators = []
 
     def readFromJson(self, fileName):
-        with open(fileName, "r") as fp:
-            buffer = json.load(fp)
-            self.minFloor = buffer["minFloor"]
-            self.maxFloor = buffer["maxFloor"]
-            for i in buffer["elevators"]:
-                id = int(i["_id"])
+        with open(fileName, "r") as filePointer:
+            buffer = json.load(filePointer)
+            self._minFloor = buffer["_minFloor"]
+            self._maxFloor = buffer["_maxFloor"]
+            for i in buffer["_elevators"]:
+                
+		id = int(i["_id"])
                 speed = float(i["_speed"])
-                minFloor = int(i["minFloor"])
-                maxFloor = int(i["maxFloor"])
+                minFloor = int(i["_minFloor"])
+                maxFloor = int(i["_maxFloor"])
                 closeTime = float(i["_closeTime"])
                 openTime = float(i["_openTime"])
                 startTime = float(i["_startTime"])
                 stopTime = float(i["_stopTime"])
-                self.elevators.append(Elevator(id,speed,minFloor,maxFloor,closeTime,openTime,startTime,stopTime))
+                
+		self._elevators.append(Elevator(id, speed, minFloor, maxFloor, closeTime, openTime, startTime, stopTime))
 
-    def __str__(self):
-        return f"This building's min floor is {self.minFloor}, max floor is {self.maxFloor} and has {len(self.elevators)} elevators"
-
-    def __repr__(self):
-        return f"This building's min floor is {self.minFloor}, max floor is {self.maxFloor} and has {len(self.elevators)} elevators"
+    def toString(self):
+        return f"Building's min floor: {self.minFloor} Max floor: {self.maxFloor} Elevators number: {len(self.elevators)}"
