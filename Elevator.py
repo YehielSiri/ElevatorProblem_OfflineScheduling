9d15505c76cb5597803f0c97627245e824bd516c
YehielSiri <57964066+YehielSiri@users.noreply.github.com>
2021-11-19 10:50:55 +0200

    Add files via upload

--- Elevator.py
+++ Elevator.py
@@ -1,16 +1,13 @@
 class Elevator:
-    def __init__(self, id, speed, minFloor, maxFloor, closeTime, openTime ,startTime ,stopTime):
+    def __init__(self, id, speed, minFloor, maxFloor, doorClosingTime, doorOpenningTime ,accelerationTime ,stopTime):
         self._id = id
         self._speed = speed
         self._minFloor = minFloor
         self._maxFloor = maxFloor
-        self._closeTime = closeTime
-        self._openTime = openTime
-        self._startTime = startTime
+        self._doorClosingTime = doorClosingTime
+        self._doorOpenningTime = doorOpenningTime
+        self._accelerationTime = accelerationTime
         self._stopTime = stopTime
 
-    def __str__(self) -> str:
-        return f"id:{self._id} ,speed:{self._speed} ,minF:{self._minFloor} ,maxF:{self._maxFloor} ,closeT:{self._closeTime} ,openT:{self._openTime} ,startT:{self._startTime} ,stopT:{self._stopTime}"
-
-    def __repr__(self) -> str:
-        return f"id:{self._id} ,speed:{self._speed} ,minF:{self._minFloor} ,maxF:{self._maxFloor} ,closeT:{self._closeTime} ,openT:{self._openTime} ,startT:{self._startTime} ,stopT:{self._stopTime}"
+    def toString(self) -> str:
+        return f"Elevator ID: {self._id} Speed: {self._speed} Min floor: {self._minFloor} Max floor: {self._maxFloor} Door closing time: {self._doorClosingTime} Door openning time: {self._doorOpenningTime} Acceleration time: {self._accelerationTime} Stop time: {self._stopTime}"
