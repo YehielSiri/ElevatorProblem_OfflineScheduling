f7fe9b1657fea753e171a53474e14cd478fc80e7
YehielSiri <57964066+YehielSiri@users.noreply.github.com>
2021-11-19 15:22:10 +0200

    Add files via upload

--- Ex1.py
+++ Ex1.py
@@ -2,6 +2,12 @@ import sys
 from LookUpgradedAlgo import *
 
 
+# to run the algorithm
+# python3 Ex1.py Ex1_Buildings/B5.json Ex1_Calls/Calls_a.csv output.csv
+# to run the tester
+# java -jar libs/Ex1_checker_V1.2_obf.jar 1111,2222,3333 data/Ex1_input/Ex1_Buildings/B5.json output.csv out.log
+
+
 buildingInJson = sys.argv[1]
 inputInCsv = sys.argv[2]
 outputInCsv = sys.argv[3]
