a9e347bd8300e43b47406a717cf374040d7e2fa6
YehielSiri <57964066+YehielSiri@users.noreply.github.com>
2021-11-19 09:14:05 +0200

    Add files via upload

--- CallForElevator.py
+++ CallForElevator.py
@@ -1,14 +1,16 @@
 class CallForElevator:
-    def __init__(self, time, src, dest, allocatedElevator):
-        self.time = time
-        self.src = src
-        self.dest = dest
-        self.allocatedElevator = allocatedElevator
-        self.length = abs(int(source) - int(destination))
+    def __init__(self, time, src, dest, status, allocatedElevator):
+        self._time = time
+        self._src = src
+        self._dest = dest
+        self._status = status
+        self._allocatedTo = allocatedTo
+	self._length = abs(int(src) - int(dest))
+
+
 
     def toString(self):
-        return ("time: " + str(self.time) + "   source: " + str(self.source) + "   destination: " + str(
-            self.destination) + "   allocatedElevatorNumber: " + str(self.allocatedElevator))
+        return f"Call received at {self.time} to service from {self.source}'th floor to {self.destination}'th floor. {self.allocatedElevator}'th elevator has allocated."
 
     def calcTime(self, elevator):
         calc = elevator.openTime * 15 + elevator.closeTime * 15 + elevator.startTime * 10 + elevator.stopTime * 10 + elevator.speed * self.absFloor + abs(
@@ -21,3 +23,16 @@ class CallForElevator:
             int(elevator.position) - int(self.source))
         calc = calc * (len(elevator.callsQueue)+15)
         return calc
+
+
+    def __str__(self):
+        return f"Call was made at {self._time}, from {self._src} to {self._dest} and it was allocated to {self._allocatedTo}"
+
+    def __repr__(self):
+        return f"Call was made at {self._time}, from {self._src} to {self._dest} and it was allocated to {self._allocatedTo}"
+
+if __name__== '__main__':
+    c1 = CallForElevator(0,1,2,-1,-1)
+    print(c1)
+    c1.from_csv("Calls_a.csv")
+    print(c1._allCalls)
